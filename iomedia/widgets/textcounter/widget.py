e92754bd21e896a83e19f617aa54d4990bdeba83
instification <jon@iomedia.co.uk>
2014-11-06 16:21:50 +0000

    Display text widget nicely

--- iomedia/widgets/textcounter/widget.py
+++ iomedia/widgets/textcounter/widget.py
@@ -1,4 +1,5 @@
 from interfaces import ITextcounterWidget
+from Products.CMFCore.utils import getToolByName
 from z3c.form.widget import Widget, FieldWidget
 from z3c.form.interfaces import IFieldWidget, IFormLayer
 from z3c.form.browser import widget
@@ -14,12 +15,12 @@ class TextcounterWidget(widget.HTMLTextAreaWidget, Widget):
     css = u'textcounter'
     value = u''
     maxlength=None
-    style=u'white-space: pre-wrap;'
 
     def update(self):
         super(TextcounterWidget, self).update()
         if self.field.max_length:
-            self.maxlength = self.field.max_length
+            self.maxlength = self.field.max_length#
+        self.transformer = getToolByName(self.context,'portal_transforms')
         widget.addFieldClass(self)
 
 @adapter(IField, IFormLayer)
